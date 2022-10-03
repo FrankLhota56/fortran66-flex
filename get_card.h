7a096f2fcb0d1fd5acef1fb9e95fa81532e26bf8
FrankLhota56 <110253892+FrankLhota56@users.noreply.github.com>
2022-10-03 09:33:00 -0400

    Improved `get_card.h` comments

--- get_card.h
+++ get_card.h
@@ -9,15 +9,15 @@
  * Cards are of fixed length, usually 80 characters. The tab character is often
  * not supported, so this function can replace tabs with spaces.
  * - If `tablen` is positive, the tab stops are located every `tablen`
- *   characters, and every tab is replaced with enough spaces to reach the next
- *   tab stop or end of card, whatever comes first.
+ *   characters, and every tab is replaced with enough blanks to reach the next
+ *   tab stop or end of card, whichever comes first.
  * - If `tablen` is zero, the tab character is treated like any other character.
  * - If a line read from `in` is shorter than `cardlen` characters long, that
- *   line will be padded on the right with spaces.
+ *   line will be padded on the right with blanks.
  * - If a line read from `in` is longer than `cardlen` characters, the line will
  *   be truncated to `cardlen` characters.
- * - If there is part of a line before the end of file, that line will be
- *   returned as a full card.
+ * - If the last line in a file is not terminated with a newline character, this
+ *   function will still a full card for that line.
  *
  * When successful, this function will store in `card` exactly `cardlen`
  * characters, followed by a newline character and a null. Make sure that the
