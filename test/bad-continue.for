bb1088fb907f24ac0566e609042a6a27f4fb802e
FrankLhota56 <110253892+FrankLhota56@users.noreply.github.com>
2022-10-11 14:52:20 -0400

    Added error test cases

---
+++ test/bad-continue.for
@@ -0,0 +1,8 @@
+C
+C Testing for misplaced Fortran 66 continue lines
+C
+C The following continue line should immediately follow an initial line or
+C another continue line
+C
+     1  / SIN (X)
+      END
